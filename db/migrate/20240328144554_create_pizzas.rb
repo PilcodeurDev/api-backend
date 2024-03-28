fed566febfe78f7b7f087642f3309259ddcddb20
simon despres <simondprs62790@hotmail.fr>
2024-03-28 16:04:33 +0100

    first-commit

---
+++ db/migrate/20240328144554_create_pizzas.rb
@@ -0,0 +1,12 @@
+class CreatePizzas < ActiveRecord::Migration[7.1]
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.integer :smallPrice
+      t.integer :mediumPrice
+      t.string :ingredient
+
+      t.timestamps
+    end
+  end
+end
