f7ca5d255a0b636853a053567f49084569d710e9
simon despres <simondprs62790@hotmail.fr>
2024-03-31 14:56:56 +0200

    add fetchdata components and catch errors

--- client/src/features/pizzas/PizzasList.jsx
+++ client/src/features/pizzas/PizzasList.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
+import { API_URL } from "../../constants";
+import fetchPizzas from "../../helper/fetchPizzas";
+
 export default function PizzasList() {
+  const [pizzas, setPizzas] = useState([]);
+  const [, setLoading] = useState(true);
+  const [, setError] = useState(null);
+
+  useEffect(() => {
+    fetchPizzas(API_URL, setPizzas, setLoading, setError);
+  }, []);
+
   return (
-    <div>PizzasList</div>
-  )
+    <>
+      <h2>Pizzas List</h2>
+      <ul>
+        {pizzas.map(({ id, name, smallPrice, mediumPrice, ingredient }) => (
+          <li key={id}>
+            <h2>{name}</h2>
+            <p>{smallPrice}</p>
+            <p>{mediumPrice}</p>
+            <p>{ingredient}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
---
+++ client/src/helper/fetchPizzas.jsx
@@ -0,0 +1,21 @@
+// Fetch pizas from the API
+
+export default function fetchPizzas(API_URL, setPizzas, setLoading, setError) {
+  async function fetchPizzas() {
+    try {
+      const response = await fetch(API_URL);
+      if (response.ok) {
+        const json = await response.json();
+        setPizzas(json);
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      setError("Error fetching pizzas");
+      console.log("Error fetching pizzas:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return fetchPizzas();
+}
