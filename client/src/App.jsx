edcdc526c727ac805dd5a115eb94db0ae5bdb82d
simon despres <simondprs62790@hotmail.fr>
2024-03-31 13:37:25 +0200

    setup react client directory

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import PizzasList from "./features/pizzas/PizzasList";
+
+function App() {
+  return (
+    <>
+      <h1>React and Rails app for learn data exchange</h1>
+      <p>FInd this application layout in client/src/App.jsx</p>
+      <PizzasList />
+    </>
+  );
+}
+
+export default App;
